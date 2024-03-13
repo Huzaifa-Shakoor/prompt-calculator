075d26727374d214bcce98ddb8b969b9d1daac0b
Muhammad Huzaifa <huzaifa2493@gmail.com>
2024-03-13 16:52:06 +0500

    Addition work done

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 function myfunction(){
     let val1 = document.getElementById("val-1").value
     let val2 = document.getElementById("val-2").value
- 
-    
+    let value1 = +val1
+    let value2 = +val2
+    const add = parseInt(eval(value1 + value2)) 
+    document.write(add)
    
 
 }
